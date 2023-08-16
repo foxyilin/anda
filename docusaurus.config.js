fcf564a5a074508b145977f0931da7bc77f0926c
Henri <henri.zhou@icloud.com>
2023-08-16 11:56:02 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -114,7 +114,7 @@ const config = {
             ],
           },
           {
-            title: 'More',
+            title: '更多',
             items: [
               {
                 label: 'Blog',
